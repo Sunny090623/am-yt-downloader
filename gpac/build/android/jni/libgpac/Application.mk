5a7735ad8f47c8f210d6d33d393d3a00435de568
Sunny <sunny081209@gmail.com>
2026-08-29 22:01:05 +0800

    add GPAC fetching step

--- gpac/build/android/jni/libgpac/Application.mk
+++
@@ -1,5 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(LOCAL_PATH)/../common.mk
-APP_MODULES := gpac
-
-#APP_CFLAGS += -Wno-shift-negative-value -Wno-pointer-sign
--- gpac/src/utils/unittests/ut_xml_parser.c
+++
@@ -1,9 +0,0 @@
-#include "tests.h"
-
-char *xml_translate_xml_string(char *str);
-unittest(xml_translate_xml_string)
-{
-    char *str= xml_translate_xml_string("&amp;");
-    assert_equal_str(str, "&");
-    gf_free(str);
-}
